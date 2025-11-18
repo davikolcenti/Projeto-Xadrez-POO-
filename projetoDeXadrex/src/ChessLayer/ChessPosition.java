b8801e532f2e4770ea5bf9b5bb459cf3bded2baa
Davi_Kolcenti <davi.kolcenti@mail.uft.edu.br>
2025-11-18 10:06:59 -0300

    Merge remote-tracking branch 'origin/main'

--- projetoDeXadrex/src/ChessLayer/ChessPosition.java
+++ projetoDeXadrex/src/ChessLayer/ChessPosition.java
@@ -1,17 +1,19 @@
 package ChessLayer;
 
+import BoardLayer.Position;
+
 public class ChessPosition {
 	protected char Column;
 	protected Integer row;
 
 	
-	position toPosition() {
+	Position toPosition() {
 		Position MyPosition = new Position(this.row, this.Column);
 		return MyPosition;
 	}
 	
-	ChessPosition fromPosition(position Position){
-	
+	ChessPosition fromPosition(Position Position){
+	return null;
 	}
 }
 
